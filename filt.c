25446e5cc82d6bda3d11dd9d02d62e3b4fca172d
Alexandre Ratchov <alex@caoua.org>
2005-04-12 17:23:43 +0200

    fixed state_lookup to take chennels into account

--- filt.c
+++ filt.c
@@ -505,7 +505,8 @@ filt_statelookup(struct filt_s *o, struct ev_s *ev) {
 	
 	if (EV_ISNOTE(ev)) {
 		for (i = &o->statelist; *i != 0; i = &(*i)->next) {
-			if ((*i)->ev.cmd == EV_NON && 
+			if ((*i)->ev.cmd == EV_NON &&
+			    (*i)->ev.data.voice.chan ==	ev->data.voice.chan && 
 			    (*i)->ev.data.voice.b0 == ev->data.voice.b0) {
 				return i;
 			}
@@ -513,13 +514,15 @@ filt_statelookup(struct filt_s *o, struct ev_s *ev) {
 	} else if (ev->cmd == EV_CTL) {
 		for (i = &o->statelist; *i != 0; i = &(*i)->next) {
 			if ((*i)->ev.cmd == EV_CTL && 
+			    (*i)->ev.data.voice.chan ==	ev->data.voice.chan && 
 			    (*i)->ev.data.voice.b0 == ev->data.voice.b0) {
 				return i;
 			}
 		}	
 	} else if (ev->cmd == EV_BEND) {
 		for (i = &o->statelist; *i != 0; i = &(*i)->next) {
-			if ((*i)->ev.cmd == EV_BEND) {
+			if ((*i)->ev.cmd == EV_BEND &&
+			    (*i)->ev.data.voice.chan ==	ev->data.voice.chan) {
 				return i;
 			}
 		}	
