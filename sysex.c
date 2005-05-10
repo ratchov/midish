5283f25fae91bfd2008a5780846d07f56b6a3491
Alexandre Ratchov <alex@caoua.org>
2005-05-11 01:43:22 +0200

    added sysex.c sysex.h (for in last commit)

---
+++ sysex.c
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2003-2005 Alexandre Ratchov
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *
+ * 	- Redistributions of source code must retain the above
+ * 	  copyright notice, this list of conditions and the
+ * 	  following disclaimer.
+ *
+ * 	- Redistributions in binary form must reproduce the above
+ * 	  copyright notice, this list of conditions and the
+ * 	  following disclaimer in the documentation and/or other
+ * 	  materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dbg.h"
+#include "sysex.h"
+#include "default.h"
+#include "pool.h"
+
+/* --------------------------------------------- sysex management --- */
+
+struct pool_s chunk_pool;
+
+void
+chunk_pool_init(unsigned size) {
+	pool_init(&chunk_pool, "chunk", sizeof(struct chunk_s), size);
+}
+
+void
+chunk_pool_done(void) {
+	pool_done(&chunk_pool);
+}
+
+
+struct chunk_s *
+chunk_new(void) {
+	struct chunk_s *o;
+	o = (struct chunk_s *)pool_new(&chunk_pool);
+	o->next = 0;
+	o->used = 0;
+	return o;
+}
+
+void
+chunk_del(struct chunk_s *o) {
+	pool_del(&chunk_pool, o);
+}
+
+
+struct pool_s sysex_pool;
+
+void
+sysex_pool_init(unsigned size) {
+	pool_init(&sysex_pool, "sysex", sizeof(struct sysex_s), size);
+}
+
+void
+sysex_pool_done(void) {
+	pool_done(&sysex_pool);
+}
+
+struct sysex_s *
+sysex_new(unsigned unit) {
+	struct sysex_s *o;
+	o = (struct sysex_s *)pool_new(&sysex_pool);
+	o->next = 0;
+	o->unit = unit;
+	o->first = o->last = 0;
+	return o;
+}
+
+void
+sysex_del(struct sysex_s *o) {
+	struct chunk_s *i, *inext;
+	for (i = o->first; i != 0; i = inext) {
+		inext = i->next;
+		chunk_del(i);
+	}
+	pool_del(&sysex_pool, o);
+}
+
+
+void
+sysex_add(struct sysex_s *o, unsigned data) {
+	struct chunk_s *ck;
+		
+	ck = o->last;
+	if (!ck) {
+		ck = o->first = o->last = chunk_new();
+	}
+	if (ck->used >= CHUNK_SIZE) {
+		ck->next = chunk_new();
+		ck = ck->next;
+		o->last = ck;
+	}
+	ck->data[ck->used++] = data;
+}
+
+void
+sysex_dbg(struct sysex_s *o) {
+	struct chunk_s *ck;
+	unsigned i;
+	dbg_puts("unit = ");
+	dbg_putx(o->unit);
+	dbg_puts(", data = { ");
+	for (ck = o->first; ck != 0; ck = ck->next) {
+		for (i = 0; i < ck->used; i++) {
+			dbg_putx(ck->data[i]);
+			dbg_puts(" ");
+		}
+	}
+	dbg_puts("}");
+}
+
+
+void
+sysexlist_init(struct sysexlist_s *o) {
+	o->first = 0;
+	o->lastptr = &o->first;
+}
+
+void
+sysexlist_done(struct sysexlist_s *o) {
+	struct sysex_s *i, *inext;
+	
+	for (i = o->first; i != 0; i = inext) {
+		inext = i->next;
+		sysex_del(i);
+	}
+	o->first = 0;
+	o->lastptr = &o->first;
+}
+
+void
+sysexlist_put(struct sysexlist_s *o, struct sysex_s *e) {
+	e->next = 0;
+	*o->lastptr = e;
+	o->lastptr = &e->next;
+}
+
+struct sysex_s *
+sysexlist_get(struct sysexlist_s *o) {
+	struct sysex_s *e;
+	if (o->first) {
+		e = o->first;
+		o->first = e->next;
+		if (e->next == 0) {
+			o->lastptr = &o->first;
+		}
+		return e;
+	}
+	return 0;		
+}
+
+void
+sysexlist_dbg(struct sysexlist_s *o) {
+	struct sysex_s *e;
+	unsigned i;
+	dbg_puts("sysex_dbg:\n");
+	for (e = o->first; e != 0; e = e->next) {
+		dbg_puts("unit = ");
+		dbg_putx(e->unit);
+		dbg_puts(", data = { ");
+		if (e->first) {
+			for (i = 0; i < e->first->used; i++) {
+				if (i > 16) {
+					dbg_puts("... ");
+					break;
+				}
+				dbg_putx(e->first->data[i]);
+				dbg_puts(" ");
+			}
+		}
+		dbg_puts("}\n");
+	}
+}
---
+++ sysex.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2003-2005 Alexandre Ratchov
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *
+ * 	- Redistributions of source code must retain the above
+ * 	  copyright notice, this list of conditions and the
+ * 	  following disclaimer.
+ *
+ * 	- Redistributions in binary form must reproduce the above
+ * 	  copyright notice, this list of conditions and the
+ * 	  following disclaimer in the documentation and/or other
+ * 	  materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SEQ_SYSEX_H
+#define SEQ_SYSEX_H
+
+struct chunk_s {
+	struct chunk_s *next;
+	unsigned used;
+#define CHUNK_SIZE	0x100
+	unsigned char data[CHUNK_SIZE];
+};
+
+struct sysex_s {
+	struct sysex_s *next;
+	unsigned unit;
+	struct chunk_s *first, *last;
+};
+
+struct sysexlist_s {
+	struct sysex_s *first, **lastptr;
+};
+
+void chunk_pool_init(unsigned size);
+void chunk_pool_done(void);
+struct chunk_s *chunk_new(void);
+void chunk_del(struct chunk_s *o);
+
+void sysex_pool_init(unsigned size);
+void sysex_pool_done(void);
+struct sysex_s *sysex_new(unsigned unit);
+void sysex_del(struct sysex_s *o);
+void sysex_add(struct sysex_s *o, unsigned data);
+void sysex_dbg(struct sysex_s *o);
+
+void sysexlist_init(struct sysexlist_s *o);
+void sysexlist_done(struct sysexlist_s *o);
+void sysexlist_put(struct sysexlist_s *o, struct sysex_s *e);
+struct sysex_s *sysexlist_get(struct sysexlist_s *o);
+void sysexlist_dbg(struct sysexlist_s *o);
+
+#endif /* SEQ_SYSEX_H */
