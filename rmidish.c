b30f3fec38a6423ae960eccf6729972c32bb724c
Alexandre Ratchov <alex@caoua.org>
2005-10-18 16:14:32 +0200

    simplify rmidish

--- rmidish.c
+++ rmidish.c
@@ -23,15 +23,25 @@
 int midish_pid;
 FILE *midish_stdout, *midish_stdin;
 
+#define LINELENGTH 10000
+char linebuf[LINELENGTH + 1];
+unsigned linenum;
+	
+void
+sighandler(int s) {
+	kill(midish_pid, s);
+}
+
 	/*
 	 * send a line to stdin of midish
 	 * just write it to the pipe
 	 */
 	 
 void
-midish_send(char *buf) {
+sendline(char *buf) {
 	fprintf(midish_stdin, "%s\n", buf);
 	fflush(midish_stdin);
+	linenum++;
 }
 
 	/*
@@ -41,10 +51,7 @@ midish_send(char *buf) {
 	 */
 
 void
-midish_wait(void) {
-#define LINELENGTH 10000
-	static char linebuf[LINELENGTH + 1];
-
+waitready(void) {
 	for (;;) {
 		if (fgets(linebuf, LINELENGTH, midish_stdout) == NULL) {
 			/* midish terminanted */
@@ -59,20 +66,19 @@ midish_wait(void) {
 	fflush(stdout);
 }
 
-
-void
-midish_interrupt(void) {
-	kill(midish_pid, SIGINT);
-}
-
 	/*
 	 * setup a pair of pipes, (stdin and stdout of midish)
 	 * and start midish.
+	 *
+	 * since midish uses SIGINT, it is also necessary
+	 * to catch SIGINT
 	 */
 
+
 void
-midish_init(void) {
+startmidish(void) {
 	int ipipe[2], opipe[2];
+	struct sigaction sa;
 	
 	/*
 	 * allocate pipes and fork
@@ -116,49 +122,38 @@ midish_init(void) {
 		perror("fdopen(midish_stdin)");
 		exit(1);
 	}
+	sa.sa_handler = sighandler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa, NULL) < 0) {
+		perror("sigaction");
+		exit(1);
+	}
+	linenum = 1;
 }
-
-
-	/*
-	 * terminate play/record/idle and terminate midish
-	 */
-
-
-void
-midish_done(void) {
-	midish_interrupt();
-	fclose(midish_stdin);
-	fclose(midish_stdout);
-}
-
-/* -------------------------------------------------------------------- */
 
 int
-main(void) {
+main() {
 #define PROMPTLENGTH 20
 	char *rl, prompt[PROMPTLENGTH];
-	unsigned linenum;
 
 	/*
 	 * if stdin or stdout is not a tty, then dont start the front end
 	 * execute midish
 	 */
+
 	if (!isatty(STDIN_FILENO) || !isatty(STDOUT_FILENO)) {
 		if (execlp(MIDISHBIN, MIDISHBIN, (char *)NULL) < 0) {
 			perror(MIDISHBIN);
 		}
 		exit(1);
 	}		
-
-	/*
-	 * initialise: start slave process 
-	 */
-	midish_init();
-	midish_wait();
 	
-	linenum = 1;
-	while(1) {
-		snprintf(prompt, PROMPTLENGTH, "%u> ", linenum);
+	startmidish();
+	
+	for (;;) {
+		waitready();
+		snprintf(prompt, PROMPTLENGTH, "%d> ", linenum);
 		rl = readline(prompt);
 		if (rl == NULL) {
 			 break;
@@ -166,11 +161,8 @@ main(void) {
 		if (rl[0] != '\0') {
 			add_history(rl);
 		}
-		midish_send(rl);
-		midish_wait();
-		linenum++;
+		sendline(rl);
+		free(rl);
 	}
-
-	midish_done();	
 	return 0;
 }
