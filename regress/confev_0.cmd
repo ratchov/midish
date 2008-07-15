7f1a5c6f7e85e89ad6761ca0f62c52d56c6468e6
Alexandre Ratchov <alex@caoua.org>
2008-07-16 00:45:19 +0200

    fix "confev" test to use new interface

--- regress/confev_0.cmd
+++ regress/confev_0.cmd
@@ -1,9 +1,9 @@
-cnew c {1 9}
-caddev {ctl  c 70 1}
-caddev {ctl  c 71 2}
-caddev {xpc  c 64 255}
-caddev {xctl c 7 1023}
-caddev {nrpn c 1 10}
-caddev {rpn  c 2 20}
-caddev {nrpn c 3 30}
-caddev {nrpn c 4 40}
+onew c {1 9}
+oaddev {ctl  c 70 1}
+oaddev {ctl  c 71 2}
+oaddev {xpc  c 64 255}
+oaddev {xctl c 7 1023}
+oaddev {nrpn c 1 10}
+oaddev {rpn  c 2 20}
+oaddev {nrpn c 3 30}
+oaddev {nrpn c 4 40}
