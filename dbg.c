97f163c0bf19ff18e637c84340951e739dc30a71
Alexandre Ratchov <alex@caoua.org>
2010-08-19 12:23:44 +0200

    bump copyright year

--- dbg.c
+++ dbg.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2007 Alexandre Ratchov <alex@caoua.org>
+ * Copyright (c) 2003-2010 Alexandre Ratchov <alex@caoua.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- mdep_alsa.c
+++ mdep_alsa.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2008 Willem van Engen <wvengen@stack.nl>
- * Copyright (c) 2003-2007 Alexandre Ratchov <alex@caoua.org>
+ * Copyright (c) 2003-2010 Alexandre Ratchov <alex@caoua.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
