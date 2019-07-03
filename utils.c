acd2cb4c770cfb39ed1e597969f75039265fb799
Alexandre Ratchov <alex@caoua.org>
2019-07-03 15:09:11 +0200

    Fix utils.h license year.

--- utils.c
+++ utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2010 Alexandre Ratchov <alex@caoua.org>
+ * Copyright (c) 2003-2012 Alexandre Ratchov <alex@caoua.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
