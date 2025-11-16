65ff783aef678db6aa415ac9dc355708695674ca
Alexandre Ratchov <alex@caoua.org>
2025-11-16 09:52:04 +0100

    Properly detect the end of the input script on Linux.
    Fixes midish hanging forever when the input is a pipe, probably in
    other cases as well. Once the other end of a pipe is closed and the
    data has been read, unlike OpenBSD, Linux wont set POLLIN anymore,
    POLLHUP must be used instead.
    
    Help from Iwan Briquemont <tracnar at gmail.com>, thanks!

--- mdep.c
+++ mdep.c
@@ -310,6 +310,15 @@ mux_mdep_wait(int docons)
 			if (revents & POLLIN)
 				cons_quit = 0;
 		} else {
+			/*
+			 * For pipes POLLHUP is set when the other end
+			 * is closed (even if there's buffered data that we
+			 * can read). So we've to ignore POLLHUP and keep
+			 * reading as long as POLLIN is set. Once all the
+			 * data has been read, unlike OpenBSD, Linux wont set
+			 * POLLIN anymore. So then we've to use POLLHUP to
+			 * detect the EOF.
+			 */
 			if (tty_pfds->revents & POLLIN) {
 				res = read(STDIN_FILENO, midibuf, MIDI_BUFSIZE);
 				if (res < 0) {
@@ -322,6 +331,9 @@ mux_mdep_wait(int docons)
 					for (i = 0; i < res; i++)
 						user_onchar(NULL, midibuf[i]);
 				}
+			} else if (tty_pfds->revents & POLLHUP) {
+				cons_eof = 1;
+				user_onchar(NULL, -1);
 			}
 		}
 	}
